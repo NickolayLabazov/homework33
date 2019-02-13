ab757cee8507f98771b751b3ee9a0e33fdd59760
Николай Лабазов <Nickolaylabazov@yandex.ru>
2019-02-13 20:33:26 +0300

    correction 3

--- test/test.js
+++ test/test.js
@@ -4,49 +4,46 @@ import getLevel from '../index';
 
 jest.mock('../http.js');
 
- beforeEach(() => {
+beforeEach(() => {
   jest.resetAllMocks();
 });
 
 test('Проверка формирования запроса', () => {
-    
-    let resp = {
-		status: 'ok',
-		level: 10
-    }     
+  const resp = {
+    status: 'ok',
+    level: 10,
+  };
 
-    fetchData.mockReturnValue(resp);
+  fetchData.mockReturnValue(resp);
 
-    getLevel(1);
-    expect(fetchData).toBeCalledWith('https://server/user/1');
-})
+  getLevel(1);
+  expect(fetchData).toBeCalledWith('https://server/user/1');
+});
 
 test('Проверка выдачи уровня', () => {
-    
-    const resp = {
-		status: 'ok',
-		level: 10
-    } 
-    
-    fetchData.mockReturnValue(resp);
-    
-    const expected = `Ваш текущий уровень: 10`;
-    const reseived = getLevel(1);
+  const resp = {
+    status: 'ok',
+    level: 10,
+  };
 
-    expect(reseived).toBe(expected);
-})
+  fetchData.mockReturnValue(resp);
+
+  const expected = 'Ваш текущий уровень: 10';
+  const reseived = getLevel(1);
+
+  expect(reseived).toBe(expected);
+});
 
 test('Проверка выдачи сообщения о недоступности уровня', () => {
-    
-    const resp = {
-		status: 'Не ok',
-		level: 10
-    }
-    
-    fetchData.mockReturnValue(resp);
-    
-    const expected = `Информация об уровне временно недоступна`;
-    const reseived = getLevel(1);
+  const resp = {
+    status: 'Не ok',
+    level: 10,
+  };
 
-    expect(reseived).toBe(expected);
-})
+  fetchData.mockReturnValue(resp);
+
+  const expected = 'Информация об уровне временно недоступна';
+  const reseived = getLevel(1);
+
+  expect(reseived).toBe(expected);
+});
